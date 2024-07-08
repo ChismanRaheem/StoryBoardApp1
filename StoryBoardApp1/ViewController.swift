e5f680b0696fee1818a9e8347950b5830ef36910
Raheem Chisman <raheem.chisman@microsoft.com>
2024-07-08 15:37:46 -0700

    Textfield / Label manipulation

--- StoryBoardApp1/ViewController.swift
+++ StoryBoardApp1/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textview: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClick(_ sender: UIButton) {
+        print ("Button Click; Good Job!")
+        textview.text = ("Button Clicked, Good Job!😎");
+    }
+    
 }
+
 
